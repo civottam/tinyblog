9b1fbbf3d3cd357e5084081017812c13d647591f
civottam <shaw.mi@outlook.com>
2018-05-18 01:35:14 +0900

    Twitter OAuth login is finished with bugs of second login.

--- tinyblog/controllers/main.py
+++ tinyblog/controllers/main.py
@@ -1,9 +1,10 @@
 from os import path
 from uuid import uuid4
-from flask import Blueprint, flash, redirect, render_template, url_for
+from flask import Blueprint, flash, redirect, render_template, url_for, request, session
 
 from tinyblog.forms import LoginForm, RegisterForm
 from tinyblog.models import db, User
+from tinyblog.extensions import twitter
 
 
 main_blueprint = Blueprint('main', __name__, template_folder=path.join(path.pardir, 'templates', 'main'))
@@ -38,3 +39,26 @@ def register():
         flash("Your user has been created, please login.", category='success')
         return redirect(url_for('main.login'))
     return render_template('register.html', form=form)
+
+
+@main_blueprint.route('/twitter')
+def twitter_login():
+    return twitter.authorize(callback=url_for('main.twitter_authorized', next=request.args.get('next') or request.referrer or None))
+
+
+@main_blueprint.route('/twitter-authorized')
+@twitter.authorized_handler
+def twitter_authorized(resp):
+    next_url = request.args.get('next') or url_for('index')
+    if resp is None:
+        flash(u'You denied the request to sign in.')
+        return redirect(next_url)
+    session['twitter_token'] = (resp['oauth_token'], resp['oauth_token_secret'])
+    session['twitter_user'] = resp['screen_name']
+    user = User.query.filter_by(username=resp['screen_name']).first()
+    if user is None:
+        user = User(id=str(uuid4()), username=resp['screen_name'], password='twitter')
+        db.session.add(user)
+        db.session.commit()
+    flash('You were signed in as {}.'.format(resp['screen_name']))
+    return redirect(next_url)
--- tinyblog/extensions.py
+++ tinyblog/extensions.py
@@ -1,3 +1,20 @@
 from flask_bcrypt import Bcrypt
+from flask_oauth import OAuth
+from flask import session
 
 bcrypt = Bcrypt()
+
+oauth = OAuth()
+
+twitter = oauth.remote_app('twitter',
+    base_url='https://api.twitter.com/1/',
+    request_token_url='https://api.twitter.com/oauth/request_token',
+    access_token_url='https://api.twitter.com/oauth/access_token',
+    authorize_url='https://api.twitter.com/oauth/authorize',
+    consumer_key='<public_key>',
+    consumer_secret='<private_key>'
+)
+
+@twitter.tokengetter
+def get_twitter_token(token=None):
+    return session.get('twitter_token')
